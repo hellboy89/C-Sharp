3342a9b10786241f2ce4c1d2eecb999b4e4ce64d
Juan Cleber <juan.fsj@gmail.com>
2021-09-14 23:53:38 -0300

    aulas sobre enumeracao

---
+++ curso_udemy_c#/diversos/exercicios_classes/ExercClasses/_teste_Enumeracoes1/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _teste_Enumeracoes1.Entities.Enums {
+    enum OrderStatus : int {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ curso_udemy_c#/diversos/exercicios_classes/ExercClasses/_teste_Enumeracoes1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using _teste_Enumeracoes1.Entities;
+using _teste_Enumeracoes1.Entities.Enums;
+
+namespace _teste_Enumeracoes1 {
+    class Program {
+        static void Main(string[] args) {
+
+            Order order = new Order {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+
+        }
+    }
+}
