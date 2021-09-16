d2a6b2175aefc108c68fd81c49669b78c6a80828
Juan Cleber <juan.fsj@gmail.com>
2021-09-15 23:26:12 -0300

    estudos de hoje sobre exercicios de enums

---
+++ curso_udemy_c#/diversos/exercicios_classes/ExercClasses/_teste_Enumeracoes2/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _teste_Enumeracoes2.Entities.Enums {
+    enum WorkerLevel : int {
+        Junior = 0,
+        MidLevel = 0,
+        Senior = 2
+    }
+}
---
+++ curso_udemy_c#/diversos/exercicios_classes/ExercClasses/_teste_Enumeracoes2/Entities/Worker.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using _teste_Enumeracoes2.Entities.Enums;
+
+namespace _teste_Enumeracoes2.Entities {
+    class Worker {
+        public string Name { get; set; }
+    }
+}
