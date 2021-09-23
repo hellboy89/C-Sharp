21708cc62afb331558c49bcbea76230bf1d18bbe
Juan Cleber <juan.fsj@gmail.com>
2021-09-22 22:14:07 -0300

    exercicios StringBuilder

---
+++ curso_udemy_c#/diversos/exercicios_classes/ExercClasses/_teste_StringBuilder1/Entities/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _teste_StringBuilder1.Entities {
+    class Comment {
+        public string Text { get; set; }
+        public Comment() {
+
+        }
+
+        public Comment(string text) {
+            Text = text;
+        }
+
+    }
+}
